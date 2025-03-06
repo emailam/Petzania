ebf4cc59218bf7f4df2680d372fa23c4d2f49ab2
Alitarek517 <102257488+Alitarek517@users.noreply.github.com>
2025-03-06 02:52:59 +0200

    Register page completed
    added folder for the Register module which will have all the pages in this module, updated Button component and completed the Register page

--- PetZania/Front-End/petzania/app/(tabs)/index.jsx
+++ PetZania/Front-End/petzania/app/(tabs)/index.jsx
@@ -5,29 +5,12 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import Button from '@/components/Button';
+import Register from '@/app/RegisterModule/Register';
 export default function HomeScreen() {
   return (
     <View>
-        <Button title="Click Me" borderRadius={10} width={200} fontSize={20} onPress={() => console.log("Button Clicked")}/>
+        
+        <Register/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
