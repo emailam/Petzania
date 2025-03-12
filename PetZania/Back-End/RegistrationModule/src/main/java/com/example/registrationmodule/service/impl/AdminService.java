f6c37d3f3e808a1aec6f97d8a48a5509bcfb559c
Amin Khaled <aminkhaledbo4@gmail.com>
2025-03-12 21:22:23 +0200

    Amin El Gen

--- PetZania/Back-End/RegistrationModule/src/main/java/com/example/registrationmodule/service/impl/AdminService.java
+++ PetZania/Back-End/RegistrationModule/src/main/java/com/example/registrationmodule/service/impl/AdminService.java
@@ -16,28 +16,33 @@ import java.util.UUID;
 @Transactional
 public class AdminService implements IAdminService {
     private final AdminRepository adminRepository;
+
     @Override
-    public boolean existsById(UUID adminId){
+    public boolean existsById(UUID adminId) {
         return adminRepository.existsById(adminId);
     }
+
     @Override
-    public Optional<Admin> getAdminById(UUID adminId){
+    public Optional<Admin> getAdminById(UUID adminId) {
         return adminRepository.findById(adminId);
     }
+
     @Override
-    public Admin partialUpdateAdminById(UUID adminId, UpdateAdminDto updatedAdmin){
+    public Admin partialUpdateAdminById(UUID adminId, UpdateAdminDto updatedAdmin) {
         return adminRepository.findById(adminId).map(existingAdmin -> {
             Optional.ofNullable(updatedAdmin.getUsername()).ifPresent(existingAdmin::setUsername);
             Optional.ofNullable(updatedAdmin.getAdminRole()).ifPresent(existingAdmin::setRole);
             return adminRepository.save(existingAdmin);
         }).orElseThrow(() -> new RuntimeException("Admin does not exist"));
     }
+
     @Override
-    public Admin saveAdmin(Admin admin){
+    public Admin saveAdmin(Admin admin) {
         return adminRepository.save(admin);
     }
+
     @Override
-    public void deleteById(UUID adminId){
+    public void deleteById(UUID adminId) {
         adminRepository.deleteById(adminId);
     }
 }
