942aee80eb00b3598a94357c57c64610d3b7e2ac
Amin Khaled <aminkhaledbo4@gmail.com>
2025-03-14 17:03:28 +0200

    Added the Redis Configuration

--- PetZania/Back-End/RegistrationModule/src/main/java/com/example/registrationmodule/RegistrationModuleApplication.java
+++ PetZania/Back-End/RegistrationModule/src/main/java/com/example/registrationmodule/RegistrationModuleApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableCaching
 public class RegistrationModuleApplication {
 
     public static void main(String[] args) {
---
+++ PetZania/Back-End/RegistrationModule/src/main/java/com/example/registrationmodule/config/RedisCacheConfig.java
@@ -0,0 +1,30 @@
+package com.example.registrationmodule.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration refreshTokenConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1)) // Refresh token cache expiration (1 day)
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .withCacheConfiguration("refreshTokens", refreshTokenConfig)
+                .build();
+    }
+}
