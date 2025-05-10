4bbd3427fe3f57ca9f4606850775869fcd2f35e8
Mohamed Khaled (Amin) <92352169+emailam@users.noreply.github.com>
2025-05-10 21:11:26 +0300

    Update UserRepository.java

--- PetZania/Back-End/friends-and-chats-module/src/main/java/com/example/friends/and/chats/module/repository/UserRepository.java
+++ PetZania/Back-End/friends-and-chats-module/src/main/java/com/example/friends/and/chats/module/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsernameIgnoreCase(String username);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    Page<User> findByUsernameStartingWithIgnoreCase(String prefix, Pageable pageable);
 }
