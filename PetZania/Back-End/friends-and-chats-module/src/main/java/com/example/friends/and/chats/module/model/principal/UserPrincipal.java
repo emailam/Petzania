9cdce497c8c10413ea08c30d0118cad1fbcff168
Mohamed Khaled (Amin) <92352169+emailam@users.noreply.github.com>
2025-05-10 21:08:20 +0300

    Added Principals for User and Admin

---
+++ PetZania/Back-End/friends-and-chats-module/src/main/java/com/example/friends/and/chats/module/model/principal/UserPrincipal.java
@@ -0,0 +1,44 @@
+package com.example.friendsAndChatsModule.model.principal;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+    private GrantedAuthority grantedAuthority;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public void setGrantedAuthority(GrantedAuthority grantedAuthority) {
+        this.grantedAuthority = grantedAuthority;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (grantedAuthority == null) {
+            return Collections.singleton(new SimpleGrantedAuthority("NONE"));
+        }
+        return Collections.singleton(grantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+}
