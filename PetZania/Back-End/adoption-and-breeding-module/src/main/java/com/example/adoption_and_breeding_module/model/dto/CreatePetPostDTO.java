197e2234300a47c063baae91a389d994805ed396
beshoy hany <beshohany142536789@gmail.com>
2025-06-15 01:46:09 +0300

    added validation to the DTOs

--- PetZania/Back-End/adoption-and-breeding-module/src/main/java/com/example/adoption_and_breeding_module/model/dto/CreatePetPostDTO.java
+++ PetZania/Back-End/adoption-and-breeding-module/src/main/java/com/example/adoption_and_breeding_module/model/dto/CreatePetPostDTO.java
@@ -1,6 +1,8 @@
 package com.example.adoption_and_breeding_module.model.dto;
 
 import com.example.adoption_and_breeding_module.model.enumeration.PetPostType;
+import com.example.adoption_and_breeding_module.validator.ValidEnum;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CreatePetPostDTO {
+    @NotNull(message = "Pet DTO is required.")
     private PetDTO petDTO;
+
     private String description;
+
+    @NotNull(message = "Post type is required.")
+    @ValidEnum(enumClass = PetPostType.class, message = "Invalid post type.")
     private PetPostType postType;
 }
