6615289ccf2d97ab37ed722943854b23a20cbcec
Amin Khaled <aminkhaledbo4@gmail.com>
2025-07-04 07:09:32 +0300

    Added SendGrid Back

--- PetZania/Back-End/registration-module/src/main/java/com/example/registrationmodule/service/impl/EmailService.java
+++ PetZania/Back-End/registration-module/src/main/java/com/example/registrationmodule/service/impl/EmailService.java
@@ -3,29 +3,60 @@ package com.example.registrationmodule.service.impl;
 import com.example.registrationmodule.exception.authenticationAndVerificattion.EmailNotSent;
 import com.example.registrationmodule.model.dto.EmailRequestDTO;
 import com.example.registrationmodule.service.IEmailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.AllArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EmailService implements IEmailService {
-    private final JavaMailSender javaMailSender;
+    private final SendGrid sendGrid;
 
     @Override
     public void sendEmail(EmailRequestDTO request) {
-        System.out.println("New Sending email service");
+        log.info("Attempting to send email to: {}", request.getTo());
+        log.info("Email subject: {}", request.getSubject());
+        log.info("Email from: {}", request.getFrom());
+        
+        Email from = new Email(request.getFrom());
+        Email to = new Email(request.getTo());
+        Content emailContent = new Content("text/plain", request.getBody());
+        String subject = request.getSubject();
+        Mail mail = new Mail(from, subject, to, emailContent);
+        mail.addHeader("X-Priority", "1");
+        Request req = new Request();
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(request.getFrom());
-            message.setTo(request.getTo());
-            message.setSubject(request.getSubject());
-            message.setText(request.getBody());
+            req.setMethod(Method.POST);
+            req.setEndpoint("mail/send");
+            req.setBody(mail.build());
             
-            javaMailSender.send(message);
-        } catch (Exception e) {
+            log.info("Sending request to SendGrid...");
+            Response response = sendGrid.api(req);
+            
+            log.info("SendGrid response status: {}", response.getStatusCode());
+            log.info("SendGrid response body: {}", response.getBody());
+            log.info("SendGrid response headers: {}", response.getHeaders());
+            
+            if (response.getStatusCode() >= 400) {
+                String errorMessage = "SendGrid API error: " + response.getStatusCode() + " - " + response.getBody();
+                log.error(errorMessage);
+                throw new EmailNotSent(errorMessage);
+            }
+            
+            log.info("Email sent successfully to: {}", request.getTo());
+        } catch (IOException e) {
             String errorMessage = "Error sending email: " + e.getMessage();
+            log.error(errorMessage, e);
             throw new EmailNotSent(errorMessage);
         }
     }
