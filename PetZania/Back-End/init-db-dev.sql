ca891e08051f8bed143401d660313e65dc70b377
Amin Khaled <aminkhaledbo4@gmail.com>
2025-09-07 11:37:43 +0300

    Created separate databases for development

---
+++ PetZania/Back-End/init-db-dev.sql
@@ -0,0 +1,5 @@
+CREATE USER development WITH PASSWORD 'development';
+CREATE DATABASE adoption_breeding_dev OWNER development;
+CREATE DATABASE friends_chat_dev OWNER development;
+CREATE DATABASE notifications_dev OWNER development;
+CREATE DATABASE registration_dev OWNER development;
